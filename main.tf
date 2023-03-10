c4d2d21345d733a40aa63b110ad23a5b48c13f15
Marians Ziemelis <122779239+marians700@users.noreply.github.com>
2023-03-10 13:30:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,43 @@ resource "aws_instance" "example" {
   ami           = "ami-047932622d41b1a1b"
   instance_type = "t3.micro"
   key_name      = "kartupelis"
-  associate_public_ip_address = true
   vpc_security_group_ids = ["sg-0dae04546b4ffb089"]
   tags = {
     Name = "Deploy Test Server"
   }
 }
-  resource "aws_eip_association" "example" {
-  instance_id   = aws_instance.example.id
-  allocation_id = "eipalloc-099d85f4e1ef5b3fd"
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [aws_instance.example]
+
+  create_duration = "30s"
+}
+resource "null_resource" "execute_commands" {
+
+  depends_on = [
+        aws_instance.example,
+        time_sleep.wait_30_seconds,
+        ]
+
+  provisioner "remote-exec" {
+    inline = [
+      var.pull_command,
+      var.deploy_command
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "admin"
+      private_key = file(var.pem_file)
+      host        = aws_instance.example.public_ip
+    }
+  }
+}
+variable "pem_file" {
+  type = string
+}
+variable "pull_command" {
+  type = string
+}
+variable "deploy_command" {
+  type = string
 }

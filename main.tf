adbc177276cddfc745cb050af3daca09660eca16
marians700 <122779239+marians700@users.noreply.github.com>
2023-05-15 17:17:30 +0300

    update deploy, deploy jenkins files and main.tf file

--- main.tf
+++ main.tf
@@ -2,13 +2,14 @@ provider "aws" {
   region = "eu-north-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-0e4f4c3e225f087ea"
+  ami           = "ami-01ed9b769ca66f1aa"
   instance_type = "t3.micro"
   key_name      = "kartupelis"
   vpc_security_group_ids = ["sg-0dae04546b4ffb089"]
   tags = {
-    Name = "Deploy Test Server"
+    Name = var.ec2_name
   }
+
 }
 resource "time_sleep" "wait_30_seconds" {
   depends_on = [aws_instance.example]
@@ -16,15 +17,29 @@ resource "time_sleep" "wait_30_seconds" {
   create_duration = "30s"
 }
 resource "null_resource" "execute_commands" {
+  triggers = {
+    pullfe_command = var.pullfe_command,
+    pullbe_command = var.pullbe_command,
+  }
 
   depends_on = [
         aws_instance.example,
         time_sleep.wait_30_seconds,
         ]
+    provisioner "file" {
+    source      = "docker-compose.yaml"
+    destination = "docker-compose.yml"
+    connection {
+      type        = "ssh"
+      user        = "admin"
+      private_key = file(var.pem_file)
+      host        = aws_instance.example.public_ip
+    }
+  }
 
   provisioner "remote-exec" {
     inline = [
-      var.pull_command,
+      var.pullfe_command,
       var.deploy_command
     ]
 
@@ -36,12 +51,19 @@ resource "null_resource" "execute_commands" {
     }
   }
 }
+resource "local_file" "jenkins_ip_file" {
+  content  = aws_instance.example.public_ip
+  filename = "ip_file"
+}
 variable "pem_file" {
   type = string
 }
-variable "pull_command" {
+variable "pullfe_command" {
   type = string
 }
 variable "deploy_command" {
+  type = string
+}
+variable "ec2_name" {
   type = string
 }

06572b0c03f37cb9879b73b31593fdce7626ea6d
marians700 <122779239+marians700@users.noreply.github.com>
2023-05-29 02:11:05 +0300

    main.tf documentation

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
+// norada pakalpojumu sniedzeju
 provider "aws" {
   region = "eu-north-1"
 }
+// norada instances parametrus
 resource "aws_instance" "example" {
   ami           = "ami-01ed9b769ca66f1aa"
   instance_type = "t3.micro"
@@ -11,6 +13,7 @@ resource "aws_instance" "example" {
   }
 
 }
+// 30 sekunzu pauze pec instances izveidosanas
 resource "time_sleep" "wait_30_seconds" {
   depends_on = [aws_instance.example]
 
@@ -18,6 +21,7 @@ resource "time_sleep" "wait_30_seconds" {
 }
 resource "null_resource" "execute_commands" {
   triggers = {
+    // norada ja sie mainigie ir mainijusies, tad sis darbibas atkartojas
     pullfe_command = var.pullfe_command,
   }
 
@@ -25,6 +29,7 @@ resource "null_resource" "execute_commands" {
         aws_instance.example,
         time_sleep.wait_30_seconds,
         ]
+        // nosuta failu uz instanci
     provisioner "file" {
     source      = "docker-compose.yaml"
     destination = "docker-compose.yml"
@@ -35,13 +40,13 @@ resource "null_resource" "execute_commands" {
       host        = aws_instance.example.public_ip
     }
   }
-
+  // izvieto docker bildi uz instances
   provisioner "remote-exec" {
     inline = [
       var.pullfe_command,
       var.deploy_command
     ]
-
+    // savienojuma konfiguracija
     connection {
       type        = "ssh"
       user        = "admin"
@@ -50,10 +55,12 @@ resource "null_resource" "execute_commands" {
     }
   }
 }
+// izveido failu ar instances ip adresi
 resource "local_file" "jenkins_ip_file" {
   content  = aws_instance.example.public_ip
   filename = "ip_file"
 }
+// norada mainigos
 variable "pem_file" {
   type = string
 }
